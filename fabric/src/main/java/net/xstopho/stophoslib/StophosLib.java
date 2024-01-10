c39cfa9daac7e5c1683339f12d3c0dcee3b2a587
Stein-N <nico-stein-98@outlook.de>
2024-01-10 20:59:33 +0100

    initial commit

---
+++ fabric/src/main/java/net/xstopho/stophoslib/StophosLib.java
@@ -0,0 +1,11 @@
+package net.xstopho.stophoslib;
+
+import net.fabricmc.api.ModInitializer;
+
+public class StophosLib implements ModInitializer {
+    
+    @Override
+    public void onInitialize() {
+        
+    }
+}
---
+++ neoforge/src/main/java/net/xstopho/stophoslib/StophosLib.java
@@ -0,0 +1,12 @@
+package net.xstopho.stophoslib;
+
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(Constants.MOD_ID)
+public class StophosLib {
+
+    public StophosLib() {
+
+    }
+}
