4b933479ef296cddb9755861d701df3b63fab91f
Stein-N <nico-stein-98@outlook.de>
2024-01-24 20:26:05 +0100

    - going back to loader specific gradle setup - changing RecipeRemainder to make it Compatible with all loader without any Mixin - Updating to latest Minecraft version - removing data and server run config from gradle setup

--- common/src/main/java/net/xstopho/stophoslib/items/RecipeRemainder.java
+++ common/src/main/java/net/xstopho/stophoslib/items/RecipeRemainder.java
@@ -1,39 +1,35 @@
 package net.xstopho.stophoslib.items;
 
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 
-public interface RecipeRemainder {
+public class RecipeRemainder {
+    public static abstract class Item extends net.minecraft.world.item.Item implements LoaderMethods {
 
-    /**
-     * Copied from Fabric API to enable the usage in the common project in
-     * Multi-Loader Projects
-     * <br><br>
-     * Returns a leftover item stack after {@code stack} is consumed in a recipe.
-     * (This is also known as "recipe remainder".)
-     * For example, using a lava bucket in a furnace as fuel will leave an empty bucket.
-     *
-     * <p>Here is an example for a recipe remainder that increments the item's damage.
-     *
-     * <pre>{@code
-     *  if (stack.getDamage() < stack.getMaxDamage() - 1) {
-     *  	ItemStack moreDamaged = stack.copy();
-     *  	moreDamaged.setDamage(stack.getDamage() + 1);
-     *  	return moreDamaged;
-     *  }
-     *
-     *  return ItemStack.EMPTY;
-     * }</pre>
-     *
-     *
-     * <p>This is a stack-aware version of {@link Item#getCraftingRemainingItem()}.
-     *
-     * <p>Note that simple item remainders can also be set via {@link Item.Properties#craftRemainder(Item)}.
-     *
-     * @param stack the consumed {@link ItemStack}
-     * @return the leftover item stack
-     */
-    default ItemStack getRecipeRemainder(ItemStack stack) {
-        return ((Item) this).hasCraftingRemainingItem() ? ((Item) this).getCraftingRemainingItem().getDefaultInstance() : ItemStack.EMPTY;
+        public Item(Properties properties) {
+            super(properties);
+        }
+
+        @Override
+        public boolean hasCraftingRemainingItem() {
+            return true;
+        }
+
+        public abstract ItemStack getRemainder(ItemStack stack);
+
+        @Override
+        public final ItemStack getRecipeRemainder(ItemStack stack) {
+            return getRemainder(stack);
+        }
+
+        @Override
+        public final ItemStack getCraftingRemainingItem(ItemStack stack) {
+            return getRemainder(stack);
+        }
+
+    }
+
+    private interface LoaderMethods {
+        ItemStack getRecipeRemainder(ItemStack stack);
+        ItemStack getCraftingRemainingItem(ItemStack stack);
     }
 }
